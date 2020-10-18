cc451929e675d010fab8d87e75b462a9ef814e9c
apnwr <65926131+apnwr@users.noreply.github.com>
2020-10-18 12:35:04 +0530

    Add files via upload
    initial commit

---
+++ ZOMATO/script.js
@@ -0,0 +1,60 @@
+// var url = "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/locations?query=New%20Delhi";
+// var key = "f1b229f942587c50e97385007941fd18";
+
+
+// fetch(url)
+//     .then((data) => {
+//         console.log(data.json());
+//         return data.json();
+// })
+
+
+// const xhr = new XMLHttpRequest();
+// const url = 'https://developers.zomato.com/api/v2.1/locations?query=New%20Delhi';
+
+// xhr.open('GET', url);
+// xhr.setRequestHeader('user-key', 'f1b229f942587c50e97385007941fd18');
+// xhr.onreadystatechange = function () {
+//     console.log("Done Done");
+// };
+// xhr.send();
+// xhr.onload = function () {
+//     // Begin accessing JSON data here
+//     var data = JSON.parse(this.response)
+//     console.log(JSON.stringify(data));
+// }
+
+const xhr = new XMLHttpRequest();
+const url = 'https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/https://developers.zomato.com/api/v2.1/collections?city_id=1';
+
+xhr.open('GET', url);
+xhr.setRequestHeader('user-key', 'f1b229f942587c50e97385007941fd18');
+xhr.onreadystatechange = function () {
+    console.log("Done Done");
+};
+xhr.send();
+xhr.onload = function () {
+    // Begin accessing JSON data here
+    
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(this.response, 'text/html');
+    document.getElementById('yea').innerHTML = this.response;
+    
+         
+   
+
+}
+ 
+//  var cont = document.createElement('div');
+//  var atg = document.createElement('a');
+//  atg.setAttribute('href', 'yupp.html');
+// atg.innerHTML = data;
+// atg.style.color = 'black';
+//  cont.append(atg);
+//  document.body.append(cont)
+ 
+
+
+
+
+
