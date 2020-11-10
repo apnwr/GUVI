9b0493c23e3ceb98263ea03b2dc81532cd1c3bd1
Ayush Panwar <ayushpanwar@Ayushs-MacBook-Air.local>
2020-11-10 18:01:00 +0530

    NodeJs 1st Task

--- Weekend TASKS/07:11:2020/index.js
+++ Weekend TASKS/07:11:2020/index.js
@@ -1,73 +1,46 @@
-const os = require('os'); // requiring the package which will be used - defining os
+
 const fs = require('fs');
+const path = require('path');
 
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log("Application is running successfully on port 3000"));
+const port = 3000;
+const dir = '/Users/ayushpanwar/Desktop/';
+
+
+app.listen(port, () => console.log("Application is running successfully on port 3000"));
 
 app.get('/home', function (req, res) {
-    res.send("Welcome to my file system"); 
+    res.send("Welcome to my file system");
 });
 
+
 app.get('/list', function (req, res) {
-    const tst = '/Users/ayushpanwar/Desktop/';
-    fs.readdir(tst, 'utf-8', (err, files) => {
+    fs.readdir(dir, 'utf-8', (err, files) => {
         if (err) throw err;
-        console.log('reading folders!');
-        let rslt = "";
-        let ico = '<img src="fileIcon.png">';
-        files.forEach(file => {
-            console.log(file);
-            rslt += '<div>' + ico + file + '</div>'
-           + '<br>';
-        })
-        res.send(rslt);
+        // fs.readFile(path.join(__dirname, "Icons/fileIcon.png"), (er, file) => {
+            console.log('reading folders!');
+            let rslt = "<ol>";
+        let ico = '';
+        
+        // let format = path.extname(file);
+        // if(format.toLowerCase() == ".png"){}
+            files.forEach(file => {
+                console.log(file);
+                let format = path.extname(dir + file);
+                if (format == ".png" || format == ".jpg") {                    
+                    ico = '<img src = "Icons/fileIcon.png">';
+                } else {
+                    ico = '<img src = "Icons/Folder-icon.png">';
+                }
+                rslt += '<li>' + ico + file + '</li> <br>';
+            })
+        rslt += '</ol>';
+            res.send("<h1> Desktop List </h1> <br>" + rslt);
+        // })
     })
-    
+
 })
 
-
-
-
-
-// const { add, sub } = require('./my-custom-package/add.js');
-
-// console.log(os.platform());
-
-// console.log(os.release());
-
-
-// //using fs package with readFile function
-// //fs.readFile(path[, options], callback)
-// fs.readFile('/Users/ayushpanwar/Desktop/NODEJS/ref.txt', 'utf-8', // giving the encoding - otherwise only buffer will be read 
-//     (err, data) => { // callback - err first as node follows the error first callback flow
-//         if (err) throw err;
-//         console.log(data);
-//     });
-
-
-// //fs.writeFile(file, data[, options], callback)
-// let info = "let's see how far we can go with the practice";
-// fs.writeFile('message.txt', info, (err) => {
-//     if (err) throw err;
-//     console.log('File has been written');
-// });
-
-// console.log(process);
-
-// let result = add(4, 5);
-// console.log(result);
-
-// let result1 = sub(15, 5);
-// console.log(result1);
-
-// const tst = '/Users/ayushpanwar/Desktop/';
-// fs.readdir(tst, 'utf-8', // giving the encoding - otherwise only buffer will be read 
-//     (err, files) => { // callback - err first as node follows the error first callback flow
-//         if(err) throw err;
-//         files.forEach(file => {
-//             console.log(file);
-//         });
-        
-//     });
+app.use("/Icons", express.static(path.join(__dirname, "Icons")));
--- Weekend TASKS/07:11:2020/my-custom-package/add.js
+++
@@ -1,9 +0,0 @@
-let add = function (value1, value2) {
-    return value1 + value2;
-};
-
-let sub = function (value1, value2) {
-    return value1 - value2;
-};
-
-module.exports = { add, sub };
